bbe48c196be053cfee12cd24d8f25d787956cfd8
Kidami13 <ulyanaboo1407@gmail.com>
2020-09-26 11:04:08 +0300

    HW_2

---
+++ HW_2.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    char a;
+    char y = 'y';
+    cin >> a;
+    if (a == y) {
+        cout << "Good";
+    }
+    else
+    {
+        cout << "Bad";
+    }
+}
+
